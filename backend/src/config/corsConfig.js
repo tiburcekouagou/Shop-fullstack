e07a72bb7bfeb5062cfb83a677fe1f2d12e77baa
Tiburce Kouagou <kouagoutiburce@gmail.com>
2025-10-17 08:45:22 +0100

    Update CORS configuration to include credentials support

--- backend/src/config/corsConfig.js
+++ backend/src/config/corsConfig.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const corsOptions = {
     origin: process.env.FRONTEND_URL,
     method: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeader: ['Content-Type', 'Authorization']
+    allowedHeader: ['Content-Type', 'Authorization'],
+    credentials: true, // Autoriser l'envoi des cookies
 }
 
 module.exports = cors(corsOptions);
