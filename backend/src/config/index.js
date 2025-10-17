aaddd98285434d73783ddaa2b7fc8529bcda007a
Tiburce Kouagou <kouagoutiburce@gmail.com>
2025-10-17 08:45:56 +0100

    Fix typo in access token secret and expiration variable names in config

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -8,9 +8,9 @@ const config = {
     mongodbUri: process.env.MONGOOSE_URI || '',
     frontendUrl: process.env.FRONTEND_URL || 'http://localhost:5173',
 
-    acessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET || 'your_jwt_access_token_secret',
+    accessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET || 'your_jwt_access_token_secret',
     refreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET || 'your_jwt_refresh_token_secret',
-    acessTokenExp: process.env.JWT_ACCESS_TOKEN_EXPIRATION || '15m',
+    accessTokenExp: process.env.JWT_ACCESS_TOKEN_EXPIRATION || '15m',
     refreshTokenExp: process.env.JWT_REFRESH_TOKEN_EXPIRATION || '30d',
 }
 
