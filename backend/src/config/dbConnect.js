01914fd42a4e8ff88950577edd71936d0f2f0210
Tiburce Kouagou <kouagoutiburce@gmail.com>
2025-10-17 08:45:43 +0100

    Refactor token.service.js for improved readability and fix access token secret reference

--- backend/src/config/dbConnect.js
+++ backend/src/config/dbConnect.js
@@ -4,7 +4,6 @@ const MONGOOSE_URI = process.env.MONGOOSE_URI;
 const connectDatabase = async () => {
     try { 
         await mongoose.connect(MONGOOSE_URI)
-        console.log('Connexion à la bdd MongoDB');
     } catch (error) {
         console.error('Erreur survenue lors de la connexion: ', error.message);
     }
--- backend/src/services/token.service.js
+++ backend/src/services/token.service.js
@@ -1,21 +1,20 @@
-const jwt = require('jsonwebtoken');
-const config = require('../config/index.js');
-const { hashValue } = require('../utils/hash.js');
-const { RefreshToken } = require('../models/refreshToken.model.js');
-const crypto = require('crypto');
+const jwt = require("jsonwebtoken");
+const config = require("../config/index.js");
+const { hashValue, verifyHash } = require("../utils/hash.js");
+const { RefreshToken } = require("../models/refreshToken.model.js");
+const crypto = require("crypto");
 
-console.log({config})
 /**
  * Créer un token d'accès (access token) JWT
  * @param {Object} payload les données à inclure dans le token (ex: { userId: user._id, role: user.role })
  * @returns {String} le token JWT
  */
-async function createAccessToken(payload) {
-    return jwt.sign(
-        payload, // e.g., { userId: user._id, role: user.role }
-        config.acessTokenSecret, // clé secrète
-        { expiresIn : config.acessTokenExp } // options (expiration, etc.
-    )
+function createAccessToken(payload) {
+  return jwt.sign(
+    payload, // e.g., { userId: user._id, role: user.role }
+    config.accessTokenSecret, // clé secrète
+    { expiresIn: config.accessTokenExp } // options (expiration, etc.
+  );
 }
 
 /**
@@ -25,22 +24,28 @@ async function createAccessToken(payload) {
  * @returns refresh token
  */
 async function createRefreshToken(userId, userAgent) {
-    const token = crypto.randomBytes(64).toString('hex'); // 128 caractères hexadécimaux
-    const jti = crypto.randomBytes(16).toString('hex'); // 32 caractères hexadécimaux
-    const expiresAt = new Date(Date.now() + timeToMs(config.refreshTokenExp)); // date d'expiration en millisecondes
-    const tokenHash = await hashValue(token);
+  // Représente le token avant hashage
+  const token = crypto.randomBytes(64).toString("hex"); // 128 caractères hexadécimaux
 
-    // Créer le le refresh token
-    await RefreshToken.create({
-        user: userId,
-        jti: jti,
-        tokenHash: tokenHash,
-        expiresAt: expiresAt,
-        userAgent: userAgent
+  // Identifiant unique du token (JWT ID)
+  const jti = crypto.randomBytes(16).toString("hex"); // 32 caractères hexadécimaux
 
-    })
+  // Date d'expiration
+  const expiresAt = new Date(Date.now() + timeToMs(config.refreshTokenExp)); // date d'expiration en millisecondes
 
-    return { token, jti, expiresAt }
+  // Hasher le token avant de le stocker en base de données
+  const tokenHash = await hashValue(token);
+
+  // Créer le le refresh token
+  await RefreshToken.create({
+    user: userId,
+    jti: jti,
+    tokenHash: tokenHash,
+    expiresAt: expiresAt,
+    userAgent: userAgent,
+  });
+
+  return { token, jti, expiresAt };
 }
 
 /**
@@ -49,11 +54,38 @@ async function createRefreshToken(userId, userAgent) {
  * @returns temps en millisecondes
  */
 function timeToMs(time) {
-    // 15m, 30d, 1h
-    if (time.endsWith('d')) return parseInt(time) * 24 * 60 * 60 * 1000; // jours
-    if (time.endsWith('h')) return parseInt(time) * 60 * 60 * 1000;
-    if (time.endsWith('m')) return parseInt(time) * 60 * 1000;
-    return parseInt(time) * 1000
+  console.log("Converting time:", time);
+  // 15m, 30d, 1h
+  if (time.endsWith("d")) return parseInt(time) * 24 * 60 * 60 * 1000; // jours
+  if (time.endsWith("h")) return parseInt(time) * 60 * 60 * 1000;
+  if (time.endsWith("m")) return parseInt(time) * 60 * 1000;
+  return parseInt(time) * 1000;
 }
 
-module.exports = { createAccessToken, createRefreshToken }
+async function renewRefreshToken(jti, token) {
+  // vérifier si le jti exist dans la BDD
+  const rt = RefreshToken.findOne({ jti });
+
+  // si le token n'existe pas ou a été révoqué
+  if (!rt || rt.revoked) throw new Error("Token invalide");
+
+  // vérifier que le token correspond au hash stocké
+  const isTokenValid = await verifyHash(rt.tokenHash, token);
+
+  if (!isTokenValid) throw new Error("Token invalide");
+
+  rt.revoked = true; // révoquer l'ancien token
+  await rt.save();
+
+  // créer un nouveau refresh token
+  const newRt = await createRefreshToken(rt.user.toString(), rt.userAgent);
+
+  return newRt;
+}
+
+module.exports = {
+  createAccessToken,
+  createRefreshToken,
+  timeToMs,
+  renewRefreshToken,
+};
